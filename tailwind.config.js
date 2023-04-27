c59f30a8d03c290caa7a14b73f0d4ced8b4d1815
StephenYu2018 <stephenyu8021@gmail.com>
2023-04-27 10:45:44 -0400

    Initial setup of tailwindcss themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ['./index.html', './src/**/*.js'],
   theme: {
+    colors: {
+      'almost-white': 'hsl(0, 0%, 98%)',
+      'medium-gray': 'hsl(0, 0%, 41%)',
+      'almost-black': 'hsl(0, 0%, 8%)',
+    },
+    fontFamily: {
+      epilogue: ['\'Epilogue\'', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
