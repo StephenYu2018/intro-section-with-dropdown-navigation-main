1c621835f464e6fa6eb39b691b865c88d37871a9
StephenYu2018 <stephenyu8021@gmail.com>
2023-05-03 09:43:43 -0400

    Add styles and toggle logic to features and company links

--- src/menu.js
+++ src/menu.js
@@ -4,6 +4,14 @@ const closeNavMenuButton = document.getElementById('close-nav-menu');
 
 const screenOverlay = document.getElementById('screen-overlay');
 
+const featuresMenu = document.getElementById('features-menu');
+const toggleFeaturesMenuIcon = document.querySelector('#toggle-features-menu > img');
+const toggleFeaturesMenu = document.getElementById('toggle-features-menu');
+
+const companyMenu = document.getElementById('company-menu');
+const toggleCompanyMenuIcon = document.querySelector('#toggle-company-menu > img');
+const toggleCompanyMenu = document.getElementById('toggle-company-menu');
+
 const offscreenNavbar = 'right-[-15rem]';
 const onscreenNavbar = 'right-0';
 const noDisplay = 'hidden';
@@ -32,4 +40,17 @@ openNavMenuButton.addEventListener('click', () => {
 
 closeNavMenuButton.addEventListener('click', hideNavbar);
 
+toggleFeaturesMenu.addEventListener('click', makeDisplayToggler(featuresMenu, toggleFeaturesMenuIcon));
+toggleCompanyMenu.addEventListener('click', makeDisplayToggler(companyMenu, toggleCompanyMenuIcon));
 
+function makeDisplayToggler(targetElement, togglerIconElement) {
+  return () => {
+    if (togglerIconElement.getAttribute('src') === './images/icon-arrow-down.svg') {
+      togglerIconElement.setAttribute('src', './images/icon-arrow-up.svg');
+    } else {
+      togglerIconElement.setAttribute('src', './images/icon-arrow-down.svg');
+    }
+
+    targetElement.classList.toggle(noDisplay);
+  };
+}
