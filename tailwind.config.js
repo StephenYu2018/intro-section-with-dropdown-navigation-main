430d91f6e3a50a5967d8fd7a68bf5bb6ff143dbe
StephenYu2018 <stephenyu8021@gmail.com>
2023-05-08 09:30:24 -0400

    Add calc(rem + vw) to all instances of vw units

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.js'],
-  theme: {
+  theme: { 
     colors: {
       'almost-white': 'hsl(0, 0%, 98%)',
       'medium-gray': 'hsl(0, 0%, 41%)',
@@ -10,7 +10,11 @@ module.exports = {
     fontFamily: {
       epilogue: ['\'Epilogue\'', 'sans-serif'],
     },
-    extend: {},
+    extend: {
+      screens: {
+        md: '800px'
+      },
+    },
   },
   plugins: [],
 }
